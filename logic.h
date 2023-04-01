26a365e3f82407172a73125b65eee99018c7a85e
michelwagner <wagm@gmx.ch>
2023-04-01 13:47:26 +0200

    initial, unit tests

---
+++ logic.h
@@ -0,0 +1,85 @@
+#pragma once
+
+class ILogicExpression
+{
+public:
+    virtual ~ILogicExpression() = default;
+    virtual bool Evaluate() = 0;
+};
+
+
+class ILogicInputData
+{
+public:
+    virtual ~ILogicInputData() = default;
+    virtual bool Get(uint32_t u32_Channel) = 0;
+};
+
+
+class CLogicInputData: public ILogicInputData
+{
+protected:
+    uint32_t mu32_Data;
+
+public:
+    virtual ~CLogicInputData() = default;
+    CLogicInputData()
+    :mu32_Data{0u}
+    {
+    };
+    
+    void Set(uint32_t u32_Data)
+    {
+        mu32_Data = u32_Data;
+    };
+
+    virtual bool Get(uint32_t u32_Channel) override
+    {
+        const uint32_t u32_ChannelMask = 1u << u32_Channel;
+        return ((mu32_Data & u32_ChannelMask) != 0u);
+    };
+};
+
+
+class CLogicInput: public ILogicExpression
+{
+protected:
+    ILogicInputData& mr_LogicInputData;
+    uint32_t mu32_Channel;
+
+public:
+    virtual ~CLogicInput() = default;
+
+    CLogicInput(ILogicInputData& r_LogicInputData, uint32_t u32_Channel)
+    :mr_LogicInputData{r_LogicInputData},
+    mu32_Channel{u32_Channel}
+    {
+    };
+
+    virtual bool Evaluate() override
+    {
+        return mr_LogicInputData.Get(mu32_Channel);
+    };
+};
+
+
+class CLogicAndOperator: public ILogicExpression
+{
+protected:
+    ILogicExpression& mr_A;
+    ILogicExpression& mr_B;
+
+public:
+    virtual ~CLogicAndOperator() = default;
+
+    CLogicAndOperator(ILogicExpression& r_A, ILogicExpression& r_B)
+    :mr_A{r_A},
+    mr_B{r_B}
+    {
+    };
+
+    virtual bool Evaluate() override
+    {
+        return mr_A.Evaluate() && mr_B.Evaluate();
+    };
+};
---
+++ logic_tests.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+#include "logic.h"
+
+TEST(logic_test, LogicInput)
+{
+    CLogicInputData LogicInputData;
+    CLogicInput LogicInput0(LogicInputData, 0u);
+    CLogicInput LogicInput1(LogicInputData, 1u);
+    CLogicInput LogicInput31(LogicInputData, 31u);
+
+    EXPECT_EQ(LogicInput0.Evaluate(), false);
+    EXPECT_EQ(LogicInput1.Evaluate(), false);
+    EXPECT_EQ(LogicInput31.Evaluate(), false);
+
+    LogicInputData.Set(0x00000001u);
+    EXPECT_EQ(LogicInput0.Evaluate(), true);
+    EXPECT_EQ(LogicInput1.Evaluate(), false);
+    EXPECT_EQ(LogicInput31.Evaluate(), false);
+
+    LogicInputData.Set(0x80000000u);
+    EXPECT_EQ(LogicInput0.Evaluate(), false);
+    EXPECT_EQ(LogicInput1.Evaluate(), false);
+    EXPECT_EQ(LogicInput31.Evaluate(), true);
+
+    LogicInputData.Set(0x80000001u);
+    EXPECT_EQ(LogicInput0.Evaluate(), true);
+    EXPECT_EQ(LogicInput1.Evaluate(), false);
+    EXPECT_EQ(LogicInput31.Evaluate(), true);
+}
+
+
+TEST(logic_test, CLogicAndOperator)
+{
+    CLogicInputData LogicInputData;
+    CLogicInput LogicInput0_H(LogicInputData, 0u);
+    CLogicInput LogicInput1_H(LogicInputData, 1u);
+    CLogicInput LogicInput2_L(LogicInputData, 2u);
+    LogicInputData.Set(0x00000003u);
+
+    CLogicAndOperator LogicAndOperator01_HH(LogicInput0_H, LogicInput1_H);
+    CLogicAndOperator LogicAndOperator02_HL(LogicInput0_H, LogicInput2_L);
+
+    EXPECT_EQ(LogicAndOperator01_HH.Evaluate(), true);
+    EXPECT_EQ(LogicAndOperator02_HL.Evaluate(), false);
+}
+
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
